600a2fa490018fbf384e9d000cceefb7e3871678
Juan Carlos Rodriguez Yarmas <jcry87@gmail.com>
2023-01-12 17:15:16 -0500

    Initial commit

---
+++ Notifications/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notifications
+//
+//  Created by Juan Carlos Rodriguez Yarmas on 12/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
